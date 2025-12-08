441135d03ee1031b4a2cf06b3098b361ae39d5b5
ntua-nasia <el18124@mail.ntua.gr>
2025-12-09 00:09:48 +0200

    Initial commit: JavaFX + Maven project setup

---
+++ src/main/java/com/example/filemanager/Main.java
@@ -0,0 +1,21 @@
+package com.example.filemanager;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello world!");
+        Scene scene = new Scene(label, 250, 50);
+        stage.setScene(scene);
+        stage.setTitle("announcement");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
